85e6f5d3c428268069d7108eb3542ad2252abc40
ADNANFILLTERINGplus <95649081+ADNANFILLTERINGplus@users.noreply.github.com>
2021-12-06 22:19:11 +0330

    Create Adnan.py

---
+++ Adnan.py
@@ -0,0 +1,134 @@
+# adnan.fill
+Adnan.fill
+## MAMAD RYSON ☘️ 
+print("adnan-fill")
+print("")
+print("vs")
+print("")
+print("abolfazl.lolo")
+print("")
+print("Hello")
+print(" ")
+print(" ")
+print("Load . . .")                 
+import os
+# AHA
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing libs
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# code colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date()
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("      GOD FILTERING")
+print (Fore.YELLOW + "")
+x = """ mohamd Ryson Loading ....
+----adnan.-------adnan-------adnan-----adnan--------adnan----adnan------adnan------adnan-----adnan . . .
+
+---------------------------------------
+                -------------------------------------------
+              ____________________adnan.plus_________________
+             _________________________________________________
+            ———————————————————————————————————————————————————
+           ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+          ≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠
+         ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+        ××××××××______×××××××××××××××××××××××××××××××××_____×××××××
+       ‡‡†‡‡‡‡‡‡|     |†‡†‡‡†‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡|     |‡††††††
+       ‡‡‡‡‡‡‡‡‡|     |‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡|     |‡††‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡|     |‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡|     |‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡_____‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡_____‡‡‡†‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡\\\\\\\\\\\\\\\\\\‡//////////////‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡\                         /‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡\                       /‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+       ‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
+        ††††††††††††††††††††††††††††††††††††††††††††††††††††††††††††
+         ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+          ________________________________________________________
+           -------------------------------------------------------
+            +++++++++++++++++++++++++++++++++++++++++++++++++++++
+             ***************************************************
+              ################################################
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+#⁶6⁶
+print(f"{red} ")
+d = """
+
+
+I Lobe You-
+ADNANFILLTERINGplus
+
+FILTER RUBIKA ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||_ _ _ _ _
+"""
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+# MAMAD*_*
+#Daly
+#          ▄▀▄   I LOVE YOU▄▀▄
+#         All Chaker Ton Mmd 
+#     
+#   
+time.sleep(4.3)
+print(f"{green} ")
+print ("Play mamad ryson - - - ! ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                       ID RUBIKA @Conquest_Rayson IM MOHAMD")
+print ("                         ID telegram @Creator_Rayson")
+print ("                          WebLog ghavanin : Immamadryson.Blogfa.com ")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("!!")
+time.sleep(0.7)
+print ("!!I")
+time.sleep(0.7)
+print ("!!!M")
+time.sleep(0.6)
+print ("!!!!M")
+time.sleep(0.5)
+print ("!!!!!M")
+time.sleep(0.4)
+print ("!!!!!!Y")
+time.sleep(0.3)
+print ("!!!!!!!S")
+time.sleep(0.2)
+print ("!!!!!!!!O")
+time.sleep(0.1)
+print ("!!!!!!!!!!N")
+time.sleep(0.5)
+print ("!!!!!!!!!!!A")
+time.sleep(0.1)
+print ("!!!!!!!!!!!!M")
+time.sleep(0.5)
